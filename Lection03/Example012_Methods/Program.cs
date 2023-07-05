ea50a6ae07b868293763e09f55eb5c5b6f0b9aa8
Kristina <misskristsi@yandex.ru>
2023-07-05 07:33:05 +0600

    Example12

---
+++ Lection03/Example012_Methods/Program.cs
@@ -0,0 +1,41 @@
+﻿//Вид1
+
+void Method1()
+{
+    Console.WriteLine("Автор...");
+}
+Method1();
+
+//Вид2
+
+void Method2(string msg)
+{
+    Console.WriteLine(msg);
+}
+Method2("Текст сообщения");
+
+//Вид 3
+
+int Method3()
+{
+    return DateTime.Now.Year;
+}
+int year = Method3();
+Console.WriteLine(year);
+
+//Вид 4
+ string Method4(int count, string text);
+ {
+    int i = 0;
+    string result = string.Empty;
+
+    while (i < count)
+    {
+        result = result + text;
+        i++;
+    }
+    return result;
+ }
+
+string res = Method4(10, "asfd");
+Console.WriteLine(res);
